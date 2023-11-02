39ba6b0a726657537075e7f115e54a6d1d51f494
Alexander Dolk <alexander@dolken.net>
2023-11-02 21:04:25 +0100

    Created simple combat UI, cleaned some scripts

---
+++ Assets/Source/UIHandlers/CombatGuiController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CombatGuiController : MonoBehaviour
+{
+    [SerializeField] private GameObject hotbar;
+    [SerializeField] private GameObject initTracker;     
+
+    private void displayUI(bool shouldDisplay)
+    {
+        hotbar.SetActive(shouldDisplay);
+        initTracker.SetActive(shouldDisplay);
+    }
+
+    private void handleGameStateChanged(GameManager.GameState state)
+    {
+        displayUI(state == GameManager.GameState.Fighting);
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnGameStateUpdated += handleGameStateChanged;
+
+        displayUI(false); //This gui shouldn't be enabled when starting game
+    }
+}
